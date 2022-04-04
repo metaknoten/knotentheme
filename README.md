e6b756cdc11ad96e2f85f38f15671462a29a75d4
friedrichweise <account@weise.io>
2022-04-04 14:01:19 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Knotentheme
+
+## Instructions
+
+Run
+
+`npm install` to setup tailwind and required plugins
+
+How to build tailwind:
+
+Development:
+
+`npm run build` for single time compiling
+
+`npm run watch` for constant development
+
+Production:
+
+`npm run prod` for production compiling. **Don't forget to turn on production mode in the theme config**# Instructions
+
+Run
+
+`npm install` to setup tailwind and required plugins
+
+How to build tailwind:
+
+Development:
+
+`npm run build` for single time compiling
+
+`npm run watch` for constant development
+
+Production:
+
+`npm run prod` for production compiling. **Don't forget to turn on production mode in the theme config**
---
+++ tailwind.config.js
@@ -0,0 +1,75 @@
+const { colors } = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: [
+    '../../config/**/*.yaml',
+    '../../pages/**/*.md',
+    './blueprints/**/*.yaml',
+    './js/**/*.js',
+    './templates/**/*.twig',
+    './knotentheme.yaml',
+    './knotentheme.php'
+  ],
+  darkMode: 'class', //false or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['"Inter"', 'system-ui']
+    },
+    extend: {
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px'
+      }
+    },
+    colors: {
+      'primary': {
+        'lighter': colors.yellow['300'],
+        DEFAULT: colors.yellow['400'],
+        'darker' : colors.yellow['500'],
+      },
+      diwoBlue: '#040a25',
+      diwoRosa: '#723465',
+      black: colors.black,
+      white: colors.white,
+      red: '#e42928',
+      green: colors.green,
+      blue: colors.blue,
+      orange: colors.orange,
+      indigo: colors.indigo,
+      transparent: 'transparent',
+      'inherit': 'inherit',
+    },
+    typography: (theme) => ({
+      DEFAULT: {
+        css: {
+          color: 'inherit',
+          lineHeight: 'inherit',
+          maxWidth: 'inherit',
+          a: {
+            transition: 'all 500ms',
+            color: theme('colors.primary.DEFAULT'),
+            '&:hover': {
+              color: theme('colors.primary.darker')
+            },
+            textDecoration: 'none'
+          },
+          strong: {
+            color: 'inherit'
+          },
+        }
+      }
+    }),
+  },
+  variants: {
+    extend: {},
+  },
+/*   plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('tailwindcss-debug-screens'),
+  ], */
+  important: false,
+}
